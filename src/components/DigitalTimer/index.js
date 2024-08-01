13443ac4328b134414f6dbaf37638c4c4960c875
Ranjith <ranjithkumar2k1@gmail.com>
2024-08-01 03:32:59 +0000

    Digital Timer

---
+++ src/components/DigitalTimer/index.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import './index.css'
+
+class DigitalTimer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Digital Timer</h1>
+        <div className="timer-card">
+          <h1> 25:00</h1>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default DigitalTimer
